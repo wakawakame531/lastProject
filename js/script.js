8b382df15abafea2aa64ccf600e82acc9e2326c8
Wakana Goto <241205079@ccmailg.meijo-u.ac.jp>
2025-07-13 18:40:20 +0900

    ｃ

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 window.addEventListener('load',function(){
     var $button = this.document.querySelector('.toggle-menu-button');
     var $menu = this.document.querySelector('.header-site-menu');
+    
     $button.addEventListener('click',function(){
         if($menu.classList.contains('is-show')){
             $menu.classList.remove('is-show');
@@ -9,6 +10,9 @@ window.addEventListener('load',function(){
             $menu.classList.add('is-show');
         }
     });
+
+    const element = this.document.querySelector('.anim-box fadeup');
+    element.classList.add('is-animated');
 });
 
 $(function(){
